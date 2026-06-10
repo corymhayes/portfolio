23b29444a41121a544fa15ae1a96cdec6a3181a2
Cory Hayes <corymhayes@gmail.com>
2026-06-10 12:45:37 -0500

    updated to adapter static

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,13 @@
-import adapter from "@sveltejs/adapter-cloudflare";
+import adapter from "@sveltejs/adapter-static";
 
 /** @type {import("@sveltejs/kit").Config} */
 const config = {
 	kit: {
-		adapter: adapter()
+		adapter: adapter({
+			pages: "dist",
+			assets: "dist",
+			fallback: "404.html"
+		})
 	}
 };
 
